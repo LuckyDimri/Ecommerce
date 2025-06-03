c9f670766e368961863b3308d53c51c5f21c3d83
LuckyDimri <shashanklucky190@gmail.com>
2025-06-03 12:15:08 +0530

    Day 1: Java Basics and Product Listing

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String []args){
+        System.out.println("Day 1 of Building my e-commerce app");
+        String product1 = "Samsung";
+        Double price1 = 999.0;
+        int stock1= 10;
+        String product2= "NOKIA";
+        Double price2 = 1000.0;
+        int stock2= 19;
+        System.out.println("Product : "+ product1 +"  price : $"+ price1 +"  stock : "+ stock1);
+        System.out.println("Product : "+ product2 +"  price : $"+ price2 +"  stock : "+ stock2);
+    }
+}
